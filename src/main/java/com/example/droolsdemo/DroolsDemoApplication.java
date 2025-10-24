ba215cb8843b57704475458686ab533cddd30740
oeyoews <jyao4783@gmail.com>
2025-10-24 10:05:50 +0800

    init project

---
+++ src/main/java/com/example/droolsdemo/DroolsDemoApplication.java
@@ -0,0 +1,13 @@
+package example.droolsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello DroolsDemoApplication");
+		SpringApplication.run(DroolsDemoApplication.class, args);
+	}
+}
